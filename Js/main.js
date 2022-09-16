6b8d0bdbef82eb36c7b9d158952e125ce03f3dd6
Philip Nnaa <codematorng@gmail.com>
2022-09-17 00:03:16 +0100

    adding responsive design

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+
+
+/*function myMenu() {
+    const mobileBtn = document.getElementById("menuCta").src='../assets/Menu-close.svg'
+    nav = document.querySelectorById("mobileMenu")
+    mobileBtn.addEventListener("click", () => {
+        nav.classlist.add("menu-btn");
+    })
+}*/
