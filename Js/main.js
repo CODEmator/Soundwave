372b56aa30a52182d996bd0ceb9847ba2dae13af
Philip Nnaa <codematorng@gmail.com>
2022-09-23 03:56:08 +0100

    adding menu blur

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,7 @@
     //select img and navigation element
     const mobileBtn = document.getElementById("menuCta");
     nav = document.querySelector("nav");
+    screenBlur = document.getElementById("blur");
 
     //toggle variable
     let toggle = true;
@@ -12,9 +13,11 @@
         if(toggle){
             mobileBtn.src = '../assets/Menu-close.svg';
             nav.classList.add("menu-btn");
+            screenBlur.classList.add("add-blur")
         }
         else{
             mobileBtn.src = '../assets/menu_icon.svg'
             nav.classList.remove("menu-btn");
+            screenBlur.classList.remove("add-blur")
         }
     });
