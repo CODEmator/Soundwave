9cdb833d9adb8fd73a0fe970188771a1a21c92ed
Philip Nnaa <codematorng@gmail.com>
2022-10-03 23:48:09 +0100

    Login__bar

--- Js/main.js
+++ Js/main.js
@@ -18,25 +18,25 @@
         else{
             mobileBtn.src = '../assets/menu_icon.svg'
             menubar.classList.remove("menu-btn");
-            screenBlur.classList.remove("add-blur")
+            screenBlur.classList.remove("add-blur");
         }
     });
     
     
-        //select login menu button and signin section
-    // const loginBtn = document.getElementById("logIn");
-    // loginBar = document.querySelector('section');
+    //select login menu button and signin section
+    const loginBtn = document.getElementById("logIn");
+    loginBar = document.querySelector('#login__Bar');
 
-    // let toggle = true;
-
-    //     //events on login click
-    // loginBtn.addEventListener("click", () => {
-    //     toggle = !toggle;
-    //         if(toggle){
-    //         loginBar.classList.add("show__signin");
-    //     }
-    //     else{
-    //         loginBar.classList.remove("show__signin")
-    //     }
-    // });
+        //events on login click
+    loginBtn.addEventListener("click", () => {
+        toggle = !toggle;
+        if(toggle){
+            loginBar.classList.add("show__signin");
+            screenBlur.classList.add("add-blur");
+        }
+        else{
+            loginBar.classList.remove("show__signin");
+            screenBlur.classList.remove("add-blur");
+        }
+    });
 
